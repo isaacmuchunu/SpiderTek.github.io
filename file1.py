d0b35a11aeb51053db5612fe2513e5ecd8ecaec3
ZED <zed@localhost.localdomain>
2017-06-19 03:13:04 +0530

    1st python lesson

---
+++ file1.py
@@ -0,0 +1,20 @@
+#strings
+s1= "hi bruh"
+s2="how was ua day"
+#string concatenation
+s3= s1+ s2
+print s3
+#lists
+my_list=[1,8,3]
+print my_list
+li2=[4,2,6]
+print li2
+#list concatenation
+glist = my_list + li2
+#list sorting
+glist.sort()
+print glist[1:3]
+#tuples
+hack = (1,2,4,5,6,7,8,9,90)
+#tuple slicing
+print hack[0:4]
